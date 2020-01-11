b3def18c693f6db51ecf3926a58419d74b3d83c9
andrew hsieh <andrewhsieh1208@gmail.com>
2020-01-11 17:13:11 -0500

    request type wraps incoming requests

--- src/types/__tests__/request.test.js
+++ src/types/__tests__/request.test.js
@@ -1,3 +1,7 @@
-test("initial test", () => {
-  expect(1+1).toEqual(2)
+const Request = require("../request.js")
+
+test("finding the ip from a request", () => {
+  const req = { ip: "123" }
+  const request = new Request(req)
+  expect(request.ip()).toBe("123")
 })
--- src/types/request.js
+++ src/types/request.js
@@ -1,3 +1,15 @@
 class Request {
+  constructor(req) {
+    this.req = req
+  }
 
+  get ip() {
+    return this.findIp
+  }
+
+  findIp() {
+    return this.req.ip
+  }
 }
+
+module.exports = Request
